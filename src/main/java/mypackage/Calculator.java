ca439967766a5ea4c48b12dd43f5dc1830f66ab5
devops <devopsguy9@gmail.com>
2018-04-08 15:30:23 +0530

    Java Web Calculator

---
+++ src/main/java/mypackage/Calculator.java
@@ -0,0 +1,56 @@
+package mypackage;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Calculator extends HttpServlet
+{
+	public long addFucn(long first, long second){
+		
+		return first+second;
+	}
+	
+	public long subFucn(long first, long second){
+		
+		return second-first;
+	}
+	
+	public long mulFucn(long first, long second){
+		
+		return first*second;
+	}
+	
+	
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        try
+        {
+        response.setContentType("text/html");
+        PrintWriter out= response.getWriter();
+        int a1= Integer.parseInt(request.getParameter("n1"));
+        int a2= Integer.parseInt(request.getParameter("n2"));
+        
+        
+        
+        if(request.getParameter("r1")!=null)
+        {
+            out.println("<h1>Addition</h1>"+addFucn(a1, a2));
+        }
+        if(request.getParameter("r2")!=null)
+        {
+            out.println("<h1>Substraction</h1>"+subFucn(a1, a2));
+        }
+        if(request.getParameter("r3")!=null)
+        {
+            out.println("<h1>Multiplication</h1>"+mulFucn(a1, a2));
+        }
+        RequestDispatcher rd=request.getRequestDispatcher("/index.jsp");  
+        rd.include(request, response);  
+        }
+        catch(Exception e)
+        {
+
+        }
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Calculator</title>
+</head>
+<body>
+<h1 style="text_align=center">Calculator</h1>
+      <form action="firstHomePage" method="get">
+<label>first number:</label>
+<input type="text" name="n1" />
+<br />
+<label>Second number : </label>
+<input type="text" name="n2" />
+<br />
+ <div>
+<label>
+<input type="radio" name="r1" value="add" />addition
+<br />
+</label>
+<input type="radio" name="r2" value="sub" />subtraction 
+<br />
+<input type="radio" name="r3" value="mul" />multiplication
+<br />
+ 
+</div>
+<input type="submit" value="submit" />
+</form>
+</body>
+</html>
